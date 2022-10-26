ebc5433e4ff99ad75aae3ba831bd5c8ae2d14093
Maycol-Mohr <maycolmohr@yahoo.com.br>
2022-10-26 19:12:35 +0200

    tentando exercicio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,10 @@
-SELECT usuario.usuario_nome AS 'usuario', COUNT(historico.cancao_id) AS 'qt_de_musicas_ouvidas', 
+SELECT usuario.usuario_nome AS 'usuario', 
+COUNT(historico.cancao_id) AS 'qt_de_musicas_ouvidas', 
 ROUND(SUM(cancao.duracao_segundos)/60, 2) AS 'total_minutos'
 FROM SpotifyClone.usuario AS usuario
 INNER JOIN SpotifyClone.historico_de_reproducao AS historico 
 ON usuario.usuario_id = historico.usuario_id
 INNER JOIN SpotifyClone.cancao AS cancao 
 ON historico.cancao_id = cancao.cancao_id
-GROUP BY usuario.usuario_nome;
+GROUP BY usuario.usuario_nome
+ORDER BY usuario.usuario_nome;
